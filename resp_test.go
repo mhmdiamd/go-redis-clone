990d1378c788342c3089750e7035a52d3e5f675d
mhmdiamd <m1pro2021@mhmdiamd-2.local>
2024-09-02 05:58:49 +0700

    feat: add redis protocol parsing

---
+++ resp_test.go
@@ -0,0 +1,48 @@
+package goredisclone
+
+import (
+	"strings"
+	"testing"
+)
+
+func TesrReadResp(t *testing.T) {
+	input := "*3\r\n$5\r\nword1\r\n$5\r\nword2\r\n$5\r\nword3\r\n"
+	reader := strings.NewReader(input)
+	result, err := readArray(reader, true)
+	if err != nil {
+		t.Error("cannot parse input: %w", err)
+		return
+	}
+
+	if len(result) != 3 {
+		t.Error("result is not an array with size 3")
+		return
+	}
+
+	element0, ok := result[0].(string)
+	if !ok {
+		t.Error(`result[0] is not "word1"`)
+	}
+
+	if element0 != "word1" {
+		t.Error(`result[0] is not "word1"`)
+	}
+
+	element1, ok := result[1].(string)
+	if !ok {
+		t.Error(`result[1] is not "word1"`)
+	}
+
+	if element1 != "word2" {
+		t.Error(`result[1] is not "word1"`)
+	}
+
+	element2, ok := result[2].(string)
+	if !ok {
+		t.Error(`result[2] is not "word1"`)
+	}
+
+	if element2 != "word3" {
+		t.Error(`result[2] is not "word1"`)
+	}
+}
